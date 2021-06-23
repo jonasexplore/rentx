e46929db6d9ff71c947d62370fd3681db9eb3888
fallying <jonasexplore@gmail.com>
2021-06-23 13:38:33 -0300

    feat(accounts): created accounts module

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,31 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTOO";
+import { User } from "../../entities/User";
+import { IUserRepository } from "../IUserRepository";
+
+class UserRepository implements IUserRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UserRepository };
