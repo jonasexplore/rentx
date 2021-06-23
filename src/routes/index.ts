96002abb9f83846db0532d33473d0de4ae2f4708
fallying <jonasexplore@gmail.com>
2021-06-23 13:39:26 -0300

    feat(route): added accounts routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./caterories.routes";
 import { specificationRoutes } from "./specification.routes";
+import { userRouter } from "./users.routes";
 
 const routes = Router();
 
 routes.use("/categories", categoriesRoutes);
 routes.use("/specifications", specificationRoutes);
+routes.use("/users", userRouter);
 
 export { routes };
