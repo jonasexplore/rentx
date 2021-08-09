7110239136d92a0b836bc918377ee209c9fb35cf
fallying <jonasexplore@gmail.com>
2021-08-09 09:07:52 -0300

    fix(shared): remove host from database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,9 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   const connection = Object.assign(defaultOptions, {
-    host: process.env.NODE_ENV === "test" ? "localhost" : host,
     database:
       process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database,
   });
